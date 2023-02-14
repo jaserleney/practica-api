f87932ea9455a7f5aef25f6fa59fa8d4f5842f7b
Jasser Blanco <yaser.keny@gmail.com>
2023-02-14 01:03:22 -0500

    Commit hora fin 1:02pm Mañana se termina en la Mañana

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Hello Vue!",
+    data: [],
+  },
+
+  methods: {
+    login() {},
+    getData(url) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.data = data.results;
+        });
+    },
+  },
+
+  created() {
+    this.getData("https://randomuser.me/api/?results=5");
   },
 });
